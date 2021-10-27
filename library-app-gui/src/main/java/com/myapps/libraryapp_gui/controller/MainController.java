8e99f4e083695b73ba482204a0b4cddeb0165866
tminh2003 <60716809+tminh2003@users.noreply.github.com>
2021-10-27 09:52:14 -0500

    Update MainController.java

--- library-app-gui/src/main/java/com/myapps/libraryapp_gui/controller/MainController.java
+++ library-app-gui/src/main/java/com/myapps/libraryapp_gui/controller/MainController.java
@@ -1,7 +1,5 @@
 package com.myapps.libraryapp_gui.controller;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +7,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.myapps.libraryapp_gui.service.ActiveUserService;
 
 @Controller
 public class MainController {
@@ -25,20 +20,4 @@ public class MainController {
 		model.addAttribute("username", session.getAttribute("username"));
 		return "index";
 	}
-	
-	@RequestMapping("/test")
-	public String test(@CookieValue(value = "counter", defaultValue = "0") Long counter,
-								HttpServletResponse response, Model model) {
-		
-		counter++;
-		
-		Cookie cookie = new Cookie("counter", counter.toString());
-		cookie.setMaxAge(1000);
-		
-		response.addCookie(cookie);
-		
-		model.addAttribute("info", counter);
-		return "test";
-	}
-
 }
