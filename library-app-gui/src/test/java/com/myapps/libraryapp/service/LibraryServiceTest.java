aebcd9acbf6f2b12400c9d4d77e175ef49808ab9
tminh2003 <60716809+tminh2003@users.noreply.github.com>
2021-12-08 10:37:24 -0600

    created rollback test for library service

--- library-app-gui/src/test/java/com/myapps/libraryapp/service/LibraryServiceTest.java
+++ library-app-gui/src/test/java/com/myapps/libraryapp/service/LibraryServiceTest.java
@@ -1,6 +1,8 @@
 package com.myapps.libraryapp.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,19 +15,21 @@ public class LibraryServiceTest {
 	@Test
 	public void testRollbackOnFail(){
 		//create mock user and book services
-		BookService bookService = new BookService();
+		BookService mockBookService = mock(BookService.class);
+		BookService realBookService = new BookService();
 		LoanService loanService = new LoanService();
 		
 		String username = "test_user";
 		String bookIsbn = "123457";
 		
 		//make one of them fail to trigger rollback
+		doThrow(new RuntimeException()).when(mockBookService).setBookStateTo("OUT", bookIsbn);
 		
+		//make sure that the transaction did not finish
+		LibraryService libraryService = new LibraryService(mockBookService, loanService);
+		libraryService.checkOutBookFor(username, bookIsbn, 30);
 		
-		//make sure that the transaction does not finish
-		LibraryService libraryService = new LibraryService(bookService, loanService);
-		
-		boolean everythingIsFine = bookService.getBookByIsbn("").getCurrentStatus().equals("IN");
+		boolean everythingIsFine = realBookService.getBookByIsbn(bookIsbn).getCurrentStatus().equals("IN");
 		everythingIsFine &= loanService.getAllLoansFor(username) == null;
 		
 		assertThat(everythingIsFine);
