6d26d0d3d3d605531d0e768b606c1cdb5dc19acf
tminh2003 <60716809+tminh2003@users.noreply.github.com>
2021-12-20 09:46:27 -0600

    Update LoadDatabase.java

--- library-app-databases/src/main/java/com/myapps/libraryapp_db/repository/LoadDatabase.java
+++ library-app-databases/src/main/java/com/myapps/libraryapp_db/repository/LoadDatabase.java
@@ -22,8 +22,8 @@ class LoadDatabase {
 	CommandLineRunner initBookDatabase(BookRepository repository) {
 
 		return args -> {
-			log.info("Preloading " + repository.save(new Book("LOTR", "Tolkien", "123456", 12.00, "OUT")));
-			log.info("Preloading " + repository.save(new Book("Harry Potter", "Rowling", "123457", 12.00, "IN")));
+			log.info("Preloading " + repository.save(new Book("123456", "LOTR", "Tolkien", 12.00, "OUT")));
+			log.info("Preloading " + repository.save(new Book("123457", "Harry Potter", "Rowling", 12.00, "IN")));
 		};
 	}
 
@@ -31,8 +31,16 @@ class LoadDatabase {
 	CommandLineRunner initUserDatabase(UserRepository repository) {
 
 		return args -> {
-			log.info("Preloading " + repository.save(new User("jack_frost", "jackfrost@gmail.com", "$2a$10$D2SYPjENGV.uhdp0xqAybOj2aDsohzJSfExzeXSLG9afly.ODfDtK", "USER")));
-			log.info("Preloading " + repository.save(new User("santa_claus", "santaclaus@gmail.com", "$2a$10$D2SYPjENGV.uhdp0xqAybOj2aDsohzJSfExzeXSLG9afly.ODfDtK", "ADMIN")));
+			log.info("Preloading " + repository.save(new User(	"jack_frost", 
+																"jackfrost@gmail.com", 
+																"$2a$10$D2SYPjENGV.uhdp0xqAybOj2aDsohzJSfExzeXSLG9afly.ODfDtK", 
+																"USER", 
+																100)));
+			log.info("Preloading " + repository.save(new User(	"santa_claus", 
+																"santaclaus@gmail.com", 
+																"$2a$10$D2SYPjENGV.uhdp0xqAybOj2aDsohzJSfExzeXSLG9afly.ODfDtK", 
+																"ADMIN", 
+																100)));
 		};
 	}
 	
